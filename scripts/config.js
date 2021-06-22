3e315dee250eb240cbf4b41a55a355d204db8662
kandashi <jacobmpalmer@live.co.uk>
2021-06-22 02:40:57 +0100

    first

---
+++ scripts/config.js
@@ -0,0 +1,44 @@
+class HyperactiveConfig {
+    constructor() {
+        this.dnd5e = {
+            "@str": "data.abilities.str.value",
+            "@con": "data.abilities.con.value",
+            "@dex": "data.abilities.dex.value",
+            "@int": "data.abilities.int.value",
+            "@wis": "data.abilities.wis.value",
+            "@cha": "data.abilities.cha.value",
+            "@ac": "data.attributes.ac.value",
+            "walk" :"data.attributes.movement.walk",
+            "fly" :"data.attributes.movement.fly",
+            "climb" :"data.attributes.movement.climb",
+            "burrow" :"data.attributes.movement.burrow",
+            "swim" :"data.attributes.movement.swim",
+            "msak" : "data.bonuses.msak.attack",
+            "msdmg" : "data.bonuses.msak.damage",
+            "mwak" : "data.bonuses.mwak.attack",
+            "mwdmg" : "data.bonuses.mwak.damage",
+            "rsak" : "data.bonuses.rsak.attack",
+            "rsdmg" : "data.bonuses.rsak.damage",
+            "rwak" : "data.bonuses.rwak.attack",
+            "rwdmg" : "data.bonuses.rwak.damage",
+            "spelldc" : "data.bonuses.spell.dc",
+            "primaryValue" : "data.resources.primary.value",
+            "primaryMax" : "data.resources.primary.max",
+            "secondaryValue" : "data.resources.secondary.value",
+            "secondaryMax" : "data.resources.secondary.max",
+            "tertiaryValue" : "data.resources.tertiary.value",
+            "tertiaryMax" : "data.resources.tertiary.max",
+            "dmgImm" : "data.traits.di.value",
+            "damRes" : "data.traits.dr.value",
+            "damVul" : "data.tratis.dv.value",
+            "languages" : "data.traits.languages.value"
+        }
+        this.currentSystem = this[game.system.id]
+
+    }
+
+    backTrack(input) {
+        return Object.keys(this.currentSystem).find(key => this.currentSystem[key] === input);
+    }
+
+}
--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,22 @@
-Hooks.once('init', async function() {
-
+let HAC, HACsystem;
+Hooks.once('init', async function () {
+    HAC = new HyperactiveConfig();
 });
 
-Hooks.once('ready', async function() {
-
+Hooks.on('renderActiveEffectConfig', (app, html, object) => {
+    let keyArray = html.find("input[type='text']")
+    for (let k of keyArray) {
+        if(!k.name || !k.name.includes("changes")  || !k.name.includes("key") ) continue
+        k.value = HAC.backTrack(k.value) ?? k.value
+    }
 });
+
+Hooks.on("preUpdateActiveEffect", (data, update) => {
+    if (!update?.changes?.length) return;
+    for (let change of update.changes) {
+        if (change.key.includes("@")) {
+            change.key = HAC.currentSystem[`${change.key}`] ?? change.key
+        }
+    }
+    data.update({ update })
+})
